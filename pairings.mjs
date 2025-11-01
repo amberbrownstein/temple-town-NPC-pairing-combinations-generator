8b9617bfbe0f486d56ade1945f78717b86226a4c
Amber Brownstein <amberbrownstein@gmail.com>
2025-11-01 13:04:43 -0400

    added comments and cleaned up some code

--- pairings.mjs
+++ pairings.mjs
@@ -1,7 +1,3 @@
-// to do:
-// add weight to certain pairings and sort combos by total weight
-// maybe change data structure of validPairings to make insertion into sorted structure more efficient
-
 class Character{
     constructor(name, pairings){
         this.name = name;
@@ -11,7 +7,13 @@ class Character{
     }
 }
 
-// initialize all characters, ordering rivals in pairings array by most to least wanted pairing
+// TODO: update characters data structure from array
+// so that priority can be a property of characters
+// instead of each individual character having their own priority
+// that needs to be summed up at the end
+
+// initialize all characters, ordering rivals in pairings array
+// by most to least wanted pairing for that character
 const characters = [
     new Character('Jay', ['Thea', 'Nadia', 'Calliope']),
     new Character('Flynn', ['Daisy', 'Thea', 'Nadia']),
@@ -26,27 +28,42 @@ const characters = [
     new Character('Juniper', ['Jonah', 'Beau', 'Tristan', 'Casey']),
 ]
 
-function findPairingCombos(charactersPaired, validPairings, currCharacterIndex){   
+function findPairingCombos(charactersPaired, validPairings, currCharacterIndex){
+    // helper method to get Character by name from array of Characters
     const getRival = (charPairings, name) => 
         charPairings.find((character) => character.name == name); 
     
+    // exit condition - all male characters have been covered, and algorithm is now on Casey
     if(currCharacterIndex >= Math.ceil(charactersPaired.length / 2) - 1){
         const currChar = charactersPaired[currCharacterIndex];
         
+        // if Casey does not have exactly 1 male rival by this point,
+        // current pairings combination will be invalid
         if(currChar.rivals.length == 1){
             const currCharPairings = currChar.pairings;
 
+            // loop through potential rivals in Casey's pairings array
             for(let i = 0; i < currCharPairings.length; ++i){
                 const currRivalName = currCharPairings[i];
 
-                //should add a check if currRival not in charactersPaired
+                // all characters after Casey will be female
+                // we only want to check female rivals since
+                // all male characters will have already been covered by this point
+
+                // if given rival does not have exactly 1 assigned rival of their own by this point
+                // current pairings combination will be invalid
                 if(charactersPaired.findIndex((character) => character.name == currRivalName) > currCharacterIndex &&
-                    getRival(charactersPaired, currRivalName).rivals.length == 1){                    
-                        let newCharsPaired = JSON.parse(JSON.stringify(charactersPaired));
-                        let currCharCopy = newCharsPaired[currCharacterIndex];
-                        let currRivalCopy = getRival(newCharsPaired, currRivalName);
+                    getRival(charactersPaired, currRivalName).rivals.length == 1){
+
+                        // deep copy characters array so as to not affect other recursive threads  
+                        const charactersPairedCopy = JSON.parse(JSON.stringify(charactersPaired));
+
+                        const currCharCopy = charactersPairedCopy[currCharacterIndex];
+                        const currRivalCopy = getRival(charactersPairedCopy, currRivalName);
                         const currCharName = currCharCopy.name;
 
+                        // add current rival to Casey's assigned rivals, and vice versa
+                        // and update total score to reflect rankning of the current pairing
                         currCharCopy.rivals.push(currRivalName);
                         currCharCopy.priority += i;
                         currRivalCopy.rivals.push(currCharName);
@@ -54,63 +71,87 @@ function findPairingCombos(charactersPaired, validPairings, currCharacterIndex){
 
                         let totalScore = 0;
 
-                        for(const character of newCharsPaired)
+                        // sum up total score of current pairings combination
+                        // by adding up priority for each respective pairing per character
+                        for(const character of charactersPairedCopy)
                             totalScore += character.priority
 
-                        validPairings.push({'score': totalScore, 'characters': newCharsPaired});
+                        // add valid pairings combination to array of pairings combinations
+                        // TODO: add a lock here to prevent race condition
+                        validPairings.push({'score': totalScore, 'characters': charactersPairedCopy});
                 }
             }
         }
     }
     else{
-        let currChar = charactersPaired[currCharacterIndex];
+        const currChar = charactersPaired[currCharacterIndex];
         const currCharPairings = currChar.pairings;
 
+        // find first rival to assign to current character
+        // do not need to check for rivals already assigned
+        // because this code block only applies to male characters
+        // who cannot be assigned to each other
+        // for the initial iteration of the game
         for(let i = 0; i < currCharPairings.length - 1; ++i){
-            let currRivals = [];
+            const currRivals = [];
             const firstRivalName = currCharPairings[i];
 
+            // if current potential rival already has 2 assigned rivals,
+            // skip and move onto next rival
             if(getRival(charactersPaired, firstRivalName).rivals.length > 1)
                 continue;
-            
-            currRivals[0] = (firstRivalName);
 
+            // find second rival to assign to current character
             for(let j = i + 1; j < currCharPairings.length; ++j){
                 const secondRivalName = currCharPairings[j];
 
+                // if current potential rival already has 2 assigned rivals,
+                // skip and move onto next rival
                 if(getRival(charactersPaired, secondRivalName).rivals.length > 1)
                     continue;
 
-                currRivals[1] = (secondRivalName)
-                let newCharsPaired = JSON.parse(JSON.stringify(charactersPaired));
-                let currCharCopy = newCharsPaired[currCharacterIndex];
-                let currCharName = currCharCopy.name;
-                let firstRivalCopy = getRival(newCharsPaired, firstRivalName);
-                let secondRivalCopy = getRival(newCharsPaired, secondRivalName);
+                // deep copy characters array so as to not affect other recursive threads  
+                const charactersPairedCopy = JSON.parse(JSON.stringify(charactersPaired));
+
+                const currCharCopy = charactersPairedCopy[currCharacterIndex];
+                const currCharName = currCharCopy.name;
+                const firstRivalCopy = getRival(charactersPairedCopy, firstRivalName);
+                const secondRivalCopy = getRival(charactersPairedCopy, secondRivalName);
                 
-                currCharCopy.rivals = currRivals;
+                // set current character's rivals to the current rivals
+                currCharCopy.rivals = [firstRivalName, secondRivalName];
+
+                // and vice versa
                 firstRivalCopy.rivals.push(currCharName);
                 secondRivalCopy.rivals.push(currCharName);
 
+                // update pairings ranking for each character who just had new rivals assigned
                 currCharCopy.priority = i + j;
                 firstRivalCopy.priority += firstRivalCopy.pairings.indexOf(currCharName);
                 secondRivalCopy.priority += secondRivalCopy.pairings.indexOf(currCharName);
 
-                findPairingCombos(newCharsPaired, validPairings, currCharacterIndex + 1);
-            }            
+                // find all combinations of pairings that include the above pairings
+                findPairingCombos(charactersPairedCopy, validPairings, currCharacterIndex + 1);
+            }
         }
     }
 }
 
-let validPairings = [];
-findPairingCombos([...characters], validPairings, 0)
+const validPairings = [];
 
+// save all valid pairing combinations in validPairings array
+findPairingCombos(characters, validPairings, 0)
+
+// sort pairing combinations by total score,
+// with the lowest score corresponding to the most desirable combination of pairings
+// a perfect score would be 15
 validPairings.sort((a, b) => a.score - b.score);
 
+// print the top 20 pairing combinations
 for(let i = 0; i < validPairings.length && i < 20; ++i){
     const pairing = validPairings[i];
     console.log(`
-Combo ${i + 1}: 
+Combo ${i + 1}, Ranking ${pairing.score}: 
 `);
     
     for(let character of pairing.characters)
